cb0f8672e0ccb7e1f321cb22db38d1b1c5edaec4
vision-05 <t.frankel@outlook.com>
2022-02-05 20:55:48 +0000

    build release

--- resources/public/js/compiled/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { vishon_dev.core.init(); } catch (e) { console.error("An error occurred when calling (vishon-dev.core/init)"); throw(e); }
--- resources/public/js/compiled/cljs-runtime/vishon_dev.config.js
+++
@@ -1,4 +0,0 @@
-goog.provide('vishon_dev.config');
-vishon_dev.config.debug_QMARK_ = goog.DEBUG;
-
-//# sourceMappingURL=vishon_dev.config.js.map
