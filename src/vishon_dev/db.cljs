16d4634039b52a0ff947cc00f3bec805bb9a84d0
vision-05 <58491379+vision-05@users.noreply.github.com>
2022-08-03 18:39:08 +0000

    Update db.cljs

--- src/vishon_dev/db.cljs
+++ src/vishon_dev/db.cljs
@@ -12,8 +12,5 @@
            :projects [:div [views/nav-bar] [:h1.page "My projects"] [views/content-list ::subs/project-posts :preview]]
            :blog-posts [:div [views/nav-bar] [:h1.page "My Blog"] [views/content-list ::subs/blog-posts :preview]]
            :contact [:div [views/nav-bar] [:h1.page "Contact me"]]}
-   :blog-posts ["#Sports\n##The definition of sports is divisive and vague. Can we change that?\nComing soon\n\nvishon"
-                "#Website\n##My journey making a website\nComing soon\n\nvishon"]
-   :projects ["#bettercode\n##A code editor built in clojure\nHas a protocol for remote editing or smthn idk"
-                   "#vishon.dev\n##My website (this one)\nStill completely static for the time being"
-                   "#More stuff\n##More stuff coming soon\nI will be making more stuff in the future"]})
+   :blog-posts []
+   :projects []})
