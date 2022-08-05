2a4ccb8061d4ebd7be847d4024b846786b82a1e3
vision-05 <58491379+vision-05@users.noreply.github.com>
2022-08-05 07:17:28 +0000

    Update db.cljs

--- src/vishon_dev/db.cljs
+++ src/vishon_dev/db.cljs
@@ -8,7 +8,7 @@
    :app (fb/init)
    :items ["home" "projects" "blog-posts" "contact"]
    :cur-page :home
-   :pages {:home [:div [views/nav-bar] [:h1.page "Hi, I'm vishon"]]
+   :pages {:home [:div [views/nav-bar] [:h1.page "Hi, I'm vishon"] [:p "Sponsored by" [:a {:href "https://nick-e.com"} "nick-e.com"]]]
            :projects [:div [views/nav-bar] [:h1.page "My projects"] [views/content-list ::subs/project-posts :preview]]
            :blog-posts [:div [views/nav-bar] [:h1.page "My Blog"] [views/content-list ::subs/blog-posts :preview]]
            :contact [:div [views/nav-bar] [:h1.page "Contact me"]]}
